48c72da20b6de0602d9c793b77d0c9f9be9dcdc3
dotmaker619 <dotmaker619@outlook.com>
2021-07-28 06:36:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React With Smooth Scrolling
+
+![React with Smooth Scrolling](./images/cover.png)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## To Run
+
+- clone this repository
+- run `npm install`
+- run `npm start`
---
+++ src/Components/Navbar.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+export default class Navbar extends Component {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  render() {
+    return (
+      <nav className="nav" id="navbar">
+        <div className="nav-content">
+          <img
+            src={logo}
+            className="nav-logo"
+            alt="Logo"
+            onClick={this.scrollToTop}
+          />
+          <ul className="nav-items">
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section1"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 1
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section2"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 2
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section3"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 3
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section4"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 4
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section5"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 5
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
